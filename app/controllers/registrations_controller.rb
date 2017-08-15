b449f67274d3f9acad5078513bd6a376c5aedb0b
Janek Kamyk <jan.kamyk@netguru.pl>
2017-08-15 15:42:04 +0200

    Add delete user link

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,4 +3,9 @@ class RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
     super
   end
+
+  def destroy
+    resource.destroy
+    redirect_to root_path
+  end
 end
